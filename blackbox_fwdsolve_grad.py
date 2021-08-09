6e41a6f7ed526e2e64318367b9bac5b31f9988af
polarbear242 <kraitley@gmail.com>
2021-08-09 14:13:59 -0700

    Update blackbox_fwdsolve_grad.py

--- blackbox_fwdsolve_grad.py
+++ blackbox_fwdsolve_grad.py
@@ -16,33 +16,32 @@ class LogLikeWithGrad(tt.Op): # ????
     itypes = [tt.dvector]  # expects a vector of parameter values when called
     otypes = [tt.dscalar]  # outputs a single scalar value (the log likelihood)
 
-    def __init__(self, loglike, data, sigma, engine):
+    def __init__(self, loglike, data, engine):
 
         #Initialise the Op with various things that our log-likelihood function requires.
 
         # add inputs as class attributes
         self.likelihood = loglike
         self.data = data
-        self.sigma = sigma
+        #self.sigma = sigma
         self.engine = engine
 
         # initialise the gradient Op (below)
-        self.logpgrad = LogLikeGrad(self.likelihood, self.data, self.sigma, self.engine)
+        self.logpgrad = LogLikeGrad(self.likelihood, self.data, self.engine)
 
     def perform(self, node, inputs, outputs):
         # the method that is used when calling the Op
-        (conductivity,) = inputs  # this will contain my variables ???
-
+        (parameters,) = inputs  # this will contain my variables ???
         # call the log-likelihood function
-        logl = self.likelihood(conductivity, self.data, self.sigma, self.engine)
+        logl = self.likelihood(parameters, self.data, self.engine)
 
         outputs[0][0] = np.array(logl)  # output the log-likelihood
 
     def grad(self, inputs, g):
         # the method that calculates the gradients - it actually returns the
         # vector-Jacobian product - g[0] is a vector of parameter values
-        (conductivity,) = inputs  # our parameters
-        return [g[0] * self.logpgrad(conductivity)]
+        (parameters,) = inputs  # our parameters
+        return [g[0] * self.logpgrad(parameters)]
 
 class LogLikeGrad(tt.Op):
     """
@@ -53,7 +52,7 @@ class LogLikeGrad(tt.Op):
     itypes = [tt.dvector]
     otypes = [tt.dvector]
 
-    def __init__(self, loglike, data, sigma, engine):
+    def __init__(self, loglike, data, engine):
         """
         Initialise with various things that the function requires. Below
         are the things that are needed in this particular example.
@@ -73,18 +72,17 @@ class LogLikeGrad(tt.Op):
         # add inputs as class attributes
         self.likelihood = loglike
         self.data = data
-        self.sigma = sigma
+        #self.sigma = sigma
         self.engine = engine
 
     def perform(self, node, inputs, outputs):
-        (conductivity,) = inputs
-
+        (parameters,) = inputs
         # define version of likelihood function to pass to derivative function
         def lnlike(values):
-            return self.likelihood(values, self.data, self.sigma, self.engine)
+            return self.likelihood(values, self.data, self.engine)
 
         # calculate gradients
-        grads = gradients(conductivity, lnlike)
+        grads = gradients(parameters, lnlike)
 
         outputs[0][0] = grads
 
@@ -94,7 +92,6 @@ def my_model(conductivity, engine):
     test_cond = matlab.double(conductivity.tolist()) #converts input into usable matlab format
     forward = eng.forward_solver(test_cond) #uses forward_solver.m file in matlab, in github ; must be in python project folder
     voltages = forward['meas']
-    print(voltages)
 
     # return the numpy array
     return voltages
@@ -221,41 +218,55 @@ def gradients(vals, func, releps=1e-3, abseps=None, mineps=1e-9, reltol=1e-3,
     return grads
 
 # define your really-complicated likelihood function that uses loads of external codes
-def my_loglike(conductivity, data, sigma, engine):
+def my_loglike(parameters, data, engine, ):
 
+    conductivity = parameters[:-1]
+    print(conductivity)
+    lognoisevariance = parameters[-1]
+    sigma = np.sqrt(np.exp(lognoisevariance));
+    v_covar = np.square(sigma)*np.eye(64);
     voltages = my_model(conductivity, engine)
-    loglik = -.5*np.log(sigma) - .5 * np.log(2 * 3.14159) - .5 * np.sum((data - voltages) ** 2) #log likelihood equation
-
+    loglik = -.5*np.log(np.linalg.det(v_covar)) - (64/2) * np.log(2 * 3.14159) - .5 * (1/np.square(sigma)) * np.sum((data - voltages) ** 2) #log likelihood equation
+    #print(loglik)
     return loglik
 
 
 mat2 = spio.loadmat('C:/Users/krait/volt2data.mat', squeeze_me=True) #file output from matlab with "save", in github, replace with correct path
 data = mat2['volt2']
+covar = spio.loadmat('C:/Users/krait/covarmatrix.mat', squeeze_me=True)
+covar2 = covar['covar']
 #data = data.tolist()
-sigma = 1.2
+#sigma = 1.2
 
 # create our Op
-logl = LogLikeWithGrad(my_loglike, data, sigma, engine)
+logl = LogLikeWithGrad(my_loglike, data, engine)
 basic_model = pm.Model()
 # use PyMC3 to sample from log-likelihood
 with basic_model:
-    cond = pm.Normal("cond", mu=1, sigma=1, shape=64)
+    mu = np.ones(64, dtype=int)
+    cond = pm.MvNormal("cond", mu=mu, cov=covar2, shape=64) #correct covar data structure?
+    #cond = pm.Normal("cond", mu=1, sigma=1.2, shape=64)
+    lognoisevariance = pm.Normal("lognoisevariance", mu=0, sigma=1, shape=1)
 
     # convert conductivity to a tensor vector
-    parameter = tt.as_tensor_variable(cond)
+    condt = tt.as_tensor_variable(cond)
+    lognvt = tt.as_tensor_variable(lognoisevariance)
+    parameters = tt.concatenate([condt, lognvt])
 
     # use a DensityDist (use a lamdba function to "call" the Op) ????
-    pm.DensityDist("likelihood", lambda v: logl(v), observed={"v": parameter})
+    pm.DensityDist("likelihood", lambda v: logl(v), observed={"v": parameters})
 
-    #trace = pm.sample(10) #draw samples from posterior
+    trace = pm.sample(10, return_inferencedata=True, idata_kwargs={"density_dist_obs": False}) #draw samples from posterior
+    az.plot_trace(trace);
+    plt.show();
 
-
-map_estimate = pm.find_MAP(model=basic_model)
-map_estimate
-eng.quit()
+#map_estimate = pm.find_MAP(model=basic_model)
+#map_estimate
+engine.quit()
 
 # plot the traces
-_ = pm.traceplot(trace)
+#az.plot_trace(trace);
+#plt.show();
 
 # put the chains in an array (for later!)
 #samples_pymc3 = np.vstack((trace["m"], trace["c"])).T
